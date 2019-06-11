e100440edb5fc97a6a0a6d6eeb76563c23b5f23b
Darrell Payne <darrellpayne@earthlink.net>
2019-06-11 08:30:26 -0700

    6. Reusable Views

---
+++ NBA-Players/StatTextView.swift
@@ -0,0 +1,23 @@
+//
+//  StatTextView.swift
+//  NBA-Players
+//
+//  Created by DARRELL A PAYNE on 6/10/19.
+//  Copyright © 2019 DARRELL A PAYNE. All rights reserved.
+//
+
+import SwiftUI
+
+struct StatTextView : View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct StatTextView_Previews : PreviewProvider {
+    static var previews: some View {
+        StatTextView()
+    }
+}
+#endif
