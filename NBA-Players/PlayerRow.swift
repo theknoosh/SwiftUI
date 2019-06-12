e73a1425744900dd70adf87e460c42b043b6952c
Darrell Payne <darrellpayne@earthlink.net>
2019-06-11 18:49:24 -0700

    7. Rows`

---
+++ NBA-Players/PlayerRow.swift
@@ -0,0 +1,44 @@
+//
+//  PlayerRow.swift
+//  NBA-Players
+//
+//  Created by DARRELL A PAYNE on 6/11/19.
+//  Copyright © 2019 DARRELL A PAYNE. All rights reserved.
+//
+
+import SwiftUI
+
+struct PlayerRow : View {
+    
+    var player: Player
+    
+    var body: some View {
+        HStack {
+            Image(player.imageName).resizable()
+                .scaledToFit()
+                .clipShape(Circle())
+                .background(Circle()).foregroundColor(player.team.color)
+                .padding(.leading, 25)
+            Text(player.name)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding(.leading, 15)
+            Spacer()
+        }
+    }
+}
+
+#if DEBUG
+struct PlayerRow_Previews : PreviewProvider {
+    static var previews: some View {
+        VStack {
+            PlayerRow(player: players[0]).previewLayout(.fixed(width: 500, height: 100))
+            PlayerRow(player: players[1]).previewLayout(.fixed(width: 500, height: 100))
+            PlayerRow(player: players[2]).previewLayout(.fixed(width: 500, height: 100))
+            PlayerRow(player: players[3]).previewLayout(.fixed(width: 500, height: 100))
+            PlayerRow(player: players[4]).previewLayout(.fixed(width: 500, height: 100))
+
+        }
+    }
+}
+#endif
